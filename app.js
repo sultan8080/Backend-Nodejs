107ea885b440edce868a46301ed63e50c6b16512
Moti-Sultan <motisultannur@gmail.com>
2025-04-29 10:30:50 +0200

    Utilisation de Middleware Express avec la méthode next

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const app = express();
-app.use((req, res) => {
+app.use((req, res, next)=>{
+    console.log('Requête recue');
+    next();
+})
+app.use((req, res, next)=>{
+    res.status(201);
+    next();
+})
+app.use((req, res, next) => {
     res.json({message: "Votre requête a bien été reçu"});
+    next();
+})
+app.use((req, res) => {
+    console.log("Réponse envoyée avec succcès");
 })
 
 module.exports = app;
