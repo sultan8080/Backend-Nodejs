ce9a6cd4efc844d8f212fa256d0c03991c2bb934
Moti-Sultan <motisultannur@gmail.com>
2025-04-29 10:08:00 +0200

    npm install -g nodemon / nodemon server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('Voila la réponse du server !');
+    res.end('Voila la réponse du premier server avec nodemon ! Installation: npm install -g nodemon Ensuite: nodemon server');
 });
 
 server.listen(process.env.PORT || 3000);
